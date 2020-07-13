e8fefd426b83882461784775c475e8726b49a510
nicholasdeporzio <deporzio.n@husky.neu.edu>
2020-07-13 12:26:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-#Install homebrew                                                               
+#1 Check if you have python3
+# Open the "Terminal" app
+python3 --version
+# If you don't get a version number printed out, complete steps 2-5
+
+#2 Install homebrew                                                               
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
                                                                                 
-#Intall pyenv                                                                   
+#3 Intall pyenv                                                                   
 brew install pyenv                                                              
                                                                                 
-#Install python 3.7.3                                                           
+#4 Install python 3.7.3                                                           
 pyenv install 3.7.3                                                             
                                                                                 
-#Activate python                                                                
+#5 Activate python                                                                
 pyenv global 3.7.3                                                              
 pyenv version                                                                   
                                                                                 
